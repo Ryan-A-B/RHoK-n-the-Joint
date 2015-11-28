f350cb418b5da403a32b4f2f143b3d47fb57cbe2
elliotthilaire <elliott.hilaire@gmail.com>
2015-11-28 19:06:22 +1000

    added example routes for sinatra

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -1,11 +1,26 @@
-
 require 'sinatra'
 
+
+
+##### Example routes #####
+
+# http://localhost:9393/ 
+# returns the string 'Hello world!'
 get '/' do
   'Hello world!'
 end
 
-
+# POST http://localhost:9393/echo
+# returns the body of the post request
 post '/echo' do
   request.body.read
 end
+
+# http://localhost:9393/hi/john
+# sets the instance variable @name from the parameter in the url
+# then renders the using the template at `views/hi` 
+get '/hi/:name' do
+  @name = params[:name]
+  erb :hi
+end
+
