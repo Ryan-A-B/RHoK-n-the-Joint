c52679cbaf97f0c775b6c94d863eabecf2703a54
elliotthilaire <elliott.hilaire@gmail.com>
2015-11-28 20:19:50 +1000

    add json and pry gem

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -1,10 +1,16 @@
 require 'sinatra'
 
+require 'json'
+require 'pry'
 
+post '/answers' do
+  response = JSON.parse request.body.read
+  response.to_s
+end
 
 ##### Example routes #####
 
-# http://localhost:9393/ 
+# http://localhost:9393/
 # returns the string 'Hello world!'
 get '/' do
   'Hello world!'
@@ -18,7 +24,7 @@ end
 
 # http://localhost:9393/hi/john
 # sets the instance variable @name from the parameter in the url
-# then renders the using the template at `views/hi` 
+# then renders the using the template at `views/hi`
 get '/hi/:name' do
   @name = params[:name]
   erb :hi
